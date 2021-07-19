90d9cbbf9cc80017a1d01712662bdc4b34d945e2
Harpreet Singh <harpreet06125@gmail.com>
2021-07-19 09:30:21 +0530

    Implement Artists Who I Follow

---
+++ GigHub/Controllers/FolloweesController.cs
@@ -0,0 +1,26 @@
+﻿using GigHub.Models;
+using Microsoft.AspNet.Identity;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace GigHub.Controllers
+{
+    public class FolloweesController : Controller
+    {
+        // GET: Followees
+        private ApplicationDbContext _context;
+
+        public FolloweesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        public ActionResult Index()
+        {
+            var userId = User.Identity.GetUserId();
+            var artists = _context.Followings
+                .Where(f => f.FollowerId == userId)
+                .Select(a => a.Followee).ToList();
+            return View(artists);
+        }
+    }
+}
